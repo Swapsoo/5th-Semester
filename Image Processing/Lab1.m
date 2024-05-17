82656d4b3401e5968d60eabc609a661105f80ea4
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2024-05-17 12:46:30 +0545

    Add files via upload

---
+++ Image Processing/Lab1.m
@@ -0,0 +1,13 @@
+clc
+close all
+clear all
+A= imread('Batman.png');
+subplot(1,2,1)
+imshow(A)
+title('Color Image of Batman')
+B=rgb2gray(A);
+#figure
+subplot(1,2,2)
+imshow(B)
+title('GrayScale Image of Batman')
+
